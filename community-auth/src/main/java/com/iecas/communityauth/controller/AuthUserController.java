4cfc60a7a7f35658876cc450ddbd49d5ea96aa0d
guo_x <guo_x0315@163.com>
2025-02-06 19:53:21 +0800

    更改：更改common文件中AOP组件自动装配

--- community-auth/src/main/java/com/iecas/communityauth/controller/AuthUserController.java
+++ community-auth/src/main/java/com/iecas/communityauth/controller/AuthUserController.java
@@ -4,6 +4,8 @@ package com.iecas.communityauth.controller;
 import com.iecas.communityauth.service.AuthUserService;
 import com.iecas.communitycommon.aop.annotation.Logger;
 import com.iecas.communitycommon.common.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/user")
+@Tag(name = "用户状态信息管理", description = "用户状态信息管理相关接口")
 public class AuthUserController {
 
     /**
@@ -26,6 +29,7 @@ public class AuthUserController {
 
 
     @Logger("测试")
+    @Operation(summary = "测试接口", description = "测试")
     @GetMapping("/test")
     public CommonResult test() {
         return new CommonResult().data("this is test").success();
---
+++ community-common/src/main/java/com/iecas/communitycommon/config/CommonAopAutoConfiguration.java
@@ -0,0 +1,22 @@
+/**
+ * @Time: 2025/2/6 19:22
+ * @Author: guoxun
+ * @File: CommonAutoConfiguration
+ * @Description:
+ */
+
+package com.iecas.communitycommon.config;
+
+import com.iecas.communitycommon.aop.aspect.LogAspect;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureAfter(AopAutoConfiguration.class)
+@ConditionalOnMissingBean(LogAspect.class)
+@ComponentScan("com.iecas.communitycommon.aop")
+public class CommonAopAutoConfiguration {
+}
