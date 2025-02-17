172530b24dd5539a8a69f0b0db9e9b4c1cb1002b
guo_x <guo_x0315@163.com>
2025-02-17 15:27:04 +0800

    添加: 1. 视频微服务; 2. 文件上传微服务

---
+++ community-uploader/src/main/java/com/iecas/communityuploader/CommunityUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.iecas.communityuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityUploaderApplication.class, args);
+	}
+
+}
---
+++ community-uploader/src/test/java/com/iecas/communityuploader/CommunityUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iecas.communityuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
