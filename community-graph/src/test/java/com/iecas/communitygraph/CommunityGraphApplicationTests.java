dbb8f3f6367e9f2da0e28de705ea2adbdc746037
guox <guo_x0315@163.com>
2025-07-20 20:09:12 +0800

    添加：图片加载功能文件夹

---
+++ community-graph/src/test/java/com/iecas/communitygraph/CommunityGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iecas.communitygraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
